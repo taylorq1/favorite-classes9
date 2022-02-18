57eca354a4eca72bb1871d17bdc2616b83b681f4
Taylor Quach <quachtaylor@gmail.com>
2022-02-17 22:30:23 -0800

    Updates

---
+++ precache-manifest.af6149f484a3b3c5084cf91e37981c02.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4460cdbd8bf7c9d621b30abe5e85d566",
+    "url": "/favorite-classes9/index.html"
+  },
+  {
+    "revision": "382f406b0af20f36132d",
+    "url": "/favorite-classes9/static/css/main.ab2ce6db.chunk.css"
+  },
+  {
+    "revision": "8b8d5a007a02500c6c63",
+    "url": "/favorite-classes9/static/js/2.624f555d.chunk.js"
+  },
+  {
+    "revision": "68c47fc33538f58652f4",
+    "url": "/favorite-classes9/static/js/3.987c3a98.chunk.js"
+  },
+  {
+    "revision": "382f406b0af20f36132d",
+    "url": "/favorite-classes9/static/js/main.5bc146f5.chunk.js"
+  },
+  {
+    "revision": "11e8ab1b548d1285f087",
+    "url": "/favorite-classes9/static/js/runtime~main.ed495f6d.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/favorite-classes9/static/media/logo.06e73328.svg"
+  }
+]);
